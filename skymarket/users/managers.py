15b7d2d2bde3f520aa9559955a912b96d790a30c
Victor Volkov <vektorn1212@gmail.com>
2024-01-16 07:01:32 +0200

    updated managers.py: added custom user manager for user models (abstract base user)

--- skymarket/users/managers.py
+++ skymarket/users/managers.py
@@ -1,7 +1,44 @@
-from django.contrib.auth.models import (
-    BaseUserManager
-)
-# TODO здесь должен быть менеджер для модели Юзера.
-# TODO Поищите эту информацию в рекомендациях к проекту
+from django.contrib.auth.models import BaseUserManager
+
+
 class UserManager(BaseUserManager):
-    pass
+    """
+    Custom user manager.
+    """
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a User without superuser privileges.
+        """
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_staff', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with superuser privileges.
+        """
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Admin must have is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Admin must have is_staff=True.')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('Admin must have is_active=True.')
+
+        return self._create_user(email, password, **extra_fields)
