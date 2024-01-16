847d8e99e8e64a0836a43fd6c9578d56ee8d1970
Victor Volkov <vektorn1212@gmail.com>
2024-01-16 23:51:41 +0200

    updated permissions.py: added custom permissions for checking is admin/is owner

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,1 +1,21 @@
-# TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+    """
+    Custom permission to checking the author.
+    """
+    message = 'Вы не являетесь владельцем.'
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.author
+
+
+class IsAdmin(BasePermission):
+    """
+    Custom permission to check if the user is an administrator.
+    """
+    message = 'Данное действие доступно только администраторам проекта.'
+
+    def has_permission(self, request, view):
+        return request.user.is_admin
