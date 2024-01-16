30298a5db3a9fbca7ef4483146c92913f7be02ed
Victor Volkov <vektorn1212@gmail.com>
2024-01-16 07:09:57 +0200

    updated urls.py: added routs for user views

--- skymarket/users/urls.py
+++ skymarket/users/urls.py
@@ -1,8 +1,22 @@
-from django.urls import include, path
+from django.urls import path, include
 from djoser.views import UserViewSet
-from rest_framework.routers import SimpleRouter
-# TODO подключите UserViewSet из Djoser.views к нашим urls.py
-# TODO для этокого рекоммендуется использовать SimpleRouter
+
+from .apps import UsersConfig
+
+app_name = UsersConfig.name
 
 urlpatterns = [
+    # djoser routes
+    path('users/', UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='register'),
+    path('users/me/',
+         UserViewSet.as_view({'get': 'me', 'put': 'me', 'patch': 'me', 'delete': 'me'}),
+         name='current_user_detail'),
+    path('users/reset_password/', UserViewSet.as_view({'post': 'reset_password'}), name='reset_password'),
+    path('users/reset_password_confirm/', UserViewSet.as_view({'post': 'reset_password_confirm'}),
+         name='reset_password_confirm'),
+    path('users/set_password/', UserViewSet.as_view({'post': 'set_password'}), name='change_password'),
+    path('users/<int:id>/',
+         UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}),
+         name='user_detail'),
+    path('', include('djoser.urls.jwt')),  # for work with JWT
 ]
