0940fe48bed422ca21a6c7432f34fb8a681c7ecd
Victor Volkov <vektorn1212@gmail.com>
2024-01-16 06:59:17 +0200

    updated email.py: added custom class for sending a password reset email (djoser)

--- skymarket/users/email.py
+++ skymarket/users/email.py
@@ -1,15 +1,26 @@
 from django.contrib.auth.tokens import default_token_generator
+from djoser import utils
+from djoser.conf import settings
 from templated_mail.mail import BaseEmailMessage
 
 
-from djoser import utils
-from djoser.conf import settings
-
-# TODO Задание со звездочкой. Здесь необходимо переместиться в исходный код
-# TODO Djoser и правильно переопределит адрес сервера (в нашем случае это localhost:3000)
 class PasswordResetEmail(BaseEmailMessage):
-    template_name = "email/password_reset.html"
+    """
+    Custom class for sending a password reset email.
+    """
+    template_name = 'email/password_reset_email.html'
 
     def get_context_data(self):
+        """
+        Returns the context data for the email message.
+        """
         # PasswordResetEmail can be deleted
-        pass
+        context = super().get_context_data()
+        context['domain'] = 'localhost:3000'
+        context['site_name'] = 'Skymarket'
+
+        user = context.get("user")
+        context["uid"] = utils.encode_uid(user.pk)
+        context["token"] = default_token_generator.make_token(user)
+        context["url"] = settings.PASSWORD_RESET_CONFIRM_URL.format(**context)
+        return context
