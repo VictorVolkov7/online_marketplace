3f1e35167559d0dd88ce29fd435317606b6990fc
Victor Volkov <vektorn1212@gmail.com>
2024-01-19 21:55:12 +0200

    updated serializers.py: added in field author users information

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from users.serializers import CurrentUserSerializer
 from .models import Comment, Ad
 
 
@@ -9,13 +10,15 @@ class AdSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Ad
-        fields = ('title', 'price', 'description', 'image', 'author', 'created_at')
+        fields = ('id', 'title', 'price', 'description', 'image', 'author', 'created_at')
 
 
 class AdDetailSerializer(serializers.ModelSerializer):
     """
     Serializer for Ad detail.
     """
+    author = CurrentUserSerializer(read_only=True)
+
     class Meta:
         model = Ad
         fields = ('id', 'title', 'price', 'image', 'description', 'author', 'created_at')
@@ -25,6 +28,26 @@ class CommentSerializer(serializers.ModelSerializer):
     """
     Serializer for Comment.
     """
+    author = CurrentUserSerializer(read_only=True)
+
     class Meta:
         model = Comment
         fields = ('id', 'text', 'author', 'created_at', 'ad')
+
+
+# DRF-spectacular serializers
+class CommonDetailSerializer(serializers.Serializer):
+    """
+    Serializer for DRF-spectacular documentation.
+    Used to process status codes.
+    """
+    detail = serializers.CharField()
+
+
+class CommonDetailAndStatusSerializer(serializers.Serializer):
+    """
+    Serializer for DRF-spectacular documentation.
+    Used to process status codes.
+    """
+    status = serializers.IntegerField()
+    details = serializers.CharField()
